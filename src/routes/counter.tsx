8eb43a2071d6ee825ea9aaed5541277e772afede
Victor Li <victorli5674@yahoo.com>
2024-08-23 22:55:03 -0700

    add shadcn

--- src/routes/counter.tsx
+++ src/routes/counter.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector } from '~/store'
 
 import { increment, selectCount } from 'state/counterSlice'
+import { Button } from 'components/button'
 
 function Counter() {
   const count = useAppSelector(selectCount)
@@ -8,8 +9,8 @@ function Counter() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">{count}</h1>
-      <button onClick={() => dispatch(increment(1))}>Increment</button>
+      <h1 className="text-3xl font-extralight">{count}</h1>
+      <Button onClick={() => dispatch(increment(1))}>Increment</Button>
     </div>
   )
 }
