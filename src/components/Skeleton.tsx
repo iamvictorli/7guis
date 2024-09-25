fe30ea6ca02061634f8a1822f472334a8d8ae96f
Victor Li <victorli5674@yahoo.com>
2024-09-24 21:10:09 -0700

    skeleton for guis

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,14 +1,14 @@
 import { Container, Section, Skeleton } from '@radix-ui/themes'
 
 export function GuiSkeleton() {
-  return <Skeleton height="500px" />
+  return <Skeleton minHeight="500px" />
 }
 
 export function IndexSkeleton() {
   return (
     <Container size="2" px={{ initial: '5', sm: '0' }}>
-      <Section size={{ initial: '1', sm: '2' }} height="100dvh">
-        <Skeleton height="100%" />
+      <Section size={{ initial: '1', sm: '2' }}>
+        <Skeleton minHeight="600px" />
       </Section>
     </Container>
   )
